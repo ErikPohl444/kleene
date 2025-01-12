1c502ef424f5c3a100a2b62ef439ed5d41653032
Erik Pohl <epohl@dmigs.com>
2025-01-12 09:25:14 -0500

    added an and

--- kleene.py
+++ kleene.py
@@ -15,6 +15,9 @@ class kleene:
         """
         return '^(' + ''.join(['\\b'+str(y)+'|' for x, y in enumerate(list) if x != len(list)]) + ')'+'{1}'+'$'
 
+    def list_to_and_regex(self, list):
+        return '' + ''.join(['(?=.*'+str(y)+')' for x, y in enumerate(list) if x != len(list)]) + ''
+
 
 if __name__ == '__main__':
     k = kleene()
@@ -26,8 +29,15 @@ if __name__ == '__main__':
     z = ['Hello', 'Goodbye', 'Aloha']
     pattern_str = k.list_to_xor_regex(z)
     print(f'pattern {pattern_str}')
-    import re
     f = re.search(pattern_str, '3')
     print(f)
-        
+    z = [1, 2, 3, 4, 5]
+    pattern_str = k.list_to_and_regex(z)
+    print(f'pattern {pattern_str}')
+    f= re.search(pattern_str, '54321')
+    print(f)
+    f= re.search(pattern_str, '42513')
+    print(f)
+    f= re.search(pattern_str, '4251355555')
+    print(f)
         
